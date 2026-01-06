f49b8c6a9af76bf400545fc9b28fc11e28d92132
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2026-01-06 22:11:07 +0500

    update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../store/auth.store';
 
 const DashboardLayout = () => {
    const [isSidebarOpen, setSidebarOpen] = React.useState(false);
-
+   const { user, isLoading } = useAuthStore()
    const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
+
+   // пока проверяем токен
+   if (isLoading) return <PageLoader />
+
+   // если НЕ авторизован → логин
+   if (!user?.data?.user?._id) {
+      return <Navigate to="/signin" replace />
+   }
 
    return (
       <div id="dashboard-layout" className="flex h-screen overflow-hidden bg-dark-primary text-gray-300 font-sans">
