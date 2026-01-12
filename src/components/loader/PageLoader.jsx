2bb13b604efc3e2ff101e711bfa8251038429bb7
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2026-01-13 01:11:48 +0500

    update PageLoader.jsx

--- src/components/loader/PageLoader.jsx
+++ src/components/loader/PageLoader.jsx
@@ -7,10 +7,7 @@ const PageLoader = () => {
         {/* Outer Ring */}
         <div className="w-16 h-16 rounded-full border-4 border-gray-200 dark:border-zinc-800 border-t-red-500 dark:border-t-red-600 animate-spin"></div>
 
-        {/* Inner Pulsing Dot */}
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="w-3 h-3 bg-red-500 rounded-full animate-ping"></div>
-        </div>
+       
 
         {/* Loading Text */}
         <div className="mt-4 text-xs font-bold text-gray-400 dark:text-gray-500 uppercase tracking-widest animate-pulse">
